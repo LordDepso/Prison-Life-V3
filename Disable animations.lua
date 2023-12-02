e18eafafb736b5e0a35980c54ac744034efdbbb7
LordDepso <149268507+LordDepso@users.noreply.github.com>
2023-12-02 14:27:04 +0000

    Delete Disable animations.lua

--- Disable animations.lua
+++
@@ -1,5 +0,0 @@
--- Created by depso
-local Animations = game:GetService("ReplicatedStorage"):WaitForChild("GunAnimations")
-for _, Anim in next, Animations:GetChildren() do
-	Anim.AnimationId = ""
-end
